14e9b19d0e4915bab8523e5fc01e3adf571a4c74
erikge <erikge@qq.com>
2015-08-29 16:58:16 +0800

    add gn documentation

---
+++ gradle/plugin_java/quickstart/src/main/java/org/gradle/Main.java
@@ -0,0 +1,13 @@
+package org.gradle;
+
+/**
+ * Created by erikge on 15-6-25.
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world");
+    }
+}
